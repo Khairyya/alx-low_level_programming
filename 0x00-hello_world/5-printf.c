5b0dbe157eb595feca173bb51dfa8031ef6d9aff
khairyya <maryam.s.dikko@gmail.com>
2023-08-11 16:09:11 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - this is the second function
- *Return: 0
+ *main - this is the entry
+ *Return: always 0
  */
 int main(void)
 {
-printf("\*with proper grammar, but the outcome is a piece of art\n");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
