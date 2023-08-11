43664d65838ba20f8179967e59820abfe887df14
khairyya <maryam.s.dikko@gmail.com>
2023-08-11 16:42:18 -0700

    intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  *main - this is my third function
- *Return:always 0
+ *Return: always 0
  */
 init main(void)
 {
-printf("Size of a char: %i byte(s)\n", sizeof(char));
-printf("Size of an int: %i byte(s)\n", sizeof(int));
-printf("Size of a Long int: %i byte(s)\n", sizeof(Long int));	
-printf("Size of a Long Long int: %i byte(s)\n", sizeof(Long Long int));
-printf("Size of a float: %i byte(s)\n", sizeof(float));
-return (0);}
+printf("Size of a char: %i byte(s)", sizeof(char));
+printf("Size of an int: %i byte(s)", sizeof(int));
+printf("Size of a long int: %i byte(s)", sizeof(long int));	
+printf("Size of a long long int: %i byte(s)", sizeof(long long int));
+printf("Size of a float: %i byte(s)", sizeof(float));
+return (0);
+}
