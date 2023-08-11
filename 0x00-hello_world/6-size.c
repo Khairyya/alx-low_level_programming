5d883a0c65dc0dd4f8e67f5362ad38a32986be90
khairyya <maryam.s.dikko@gmail.com>
2023-08-11 12:41:58 -0700

    hello puts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - this is my third function
+ *Return:always 0
+ */
+init main(void)
+{
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a Long int: %i byte(s)\n", sizeof(Long int));	
+printf("Size of a Long Long int: %i byte(s)\n", sizeof(Long Long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
+return (0);}
