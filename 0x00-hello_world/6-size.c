cb132cbd3b0826fe29611e9363e1a87745c5f9c3
khairyya <maryam.s.dikko@gmail.com>
2023-08-13 10:41:30 -0700

    size adde

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,12 @@
  *main - this is my third function
  *Return: always 0
  */
-init main(void)
+int main(void)
 {
-printf("Size of a char: %i byte(s)", sizeof(char));
-printf("Size of an int: %i byte(s)", sizeof(int));
-printf("Size of a long int: %i byte(s)", sizeof(long int));	
-printf("Size of a long long int: %i byte(s)", sizeof(long long int));
-printf("Size of a float: %i byte(s)", sizeof(float));
-return (0);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
